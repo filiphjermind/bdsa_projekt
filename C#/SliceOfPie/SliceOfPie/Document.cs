739a4e8be63d480ef43f85d8649c634285efcd0b
Bergar Simonsen <bergar.simonsen@gmail.com>
2012-11-23 16:19:40 +0100

    Document and DocumentHandler core architecture

--- C#/SliceOfPie/SliceOfPie/Document.cs
+++ C#/SliceOfPie/SliceOfPie/Document.cs
@@ -7,5 +7,44 @@ namespace SliceOfPie
 {
     class Document
     {
+        /// <summary>
+        /// List of users that have access to edit the document.
+        /// </summary>
+        public List<User> users;
+
+        /// <summary>
+        /// The owner of the document.
+        /// </summary>
+        public User owner
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// The title of the document
+        /// </summary>
+        public string title
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Unique id of the document
+        /// </summary>
+        public int documentId
+        {
+            get;
+            set;
+        }
+
+        public Document(User owner, string title)
+        {
+            this.owner = owner;
+            this.title = title;
+            //this.documentId
+            this.users = new List<User>();
+        }
     }
 }
--- C#/SliceOfPie/SliceOfPie/DocumentHandler.cs
+++ C#/SliceOfPie/SliceOfPie/DocumentHandler.cs
@@ -7,5 +7,52 @@ namespace SliceOfPie
 {
     class DocumentHandler
     {
+        /// <summary>
+        /// Creates a new document.
+        /// </summary>
+        /// <param name="owner">The owner of the document</param>
+        /// <returns>The newly created document.</returns>
+        public Document NewDocument(User owner)
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// Returns all documents from the database that belong to
+        /// the specific user.
+        /// </summary>
+        /// <param name="user">Owner of the documents</param>
+        /// <returns>List of all documents that belong to the user.</returns>
+        public List<Document> GetAllDocuments(User user)
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// Saves a document to the database.
+        /// </summary>
+        /// <param name="doc">The document to save</param>
+        public void SaveDocument(Document doc)
+        { 
+        
+        }
+
+        /// <summary>
+        /// Opens a document based on the document id.
+        /// </summary>
+        /// <param name="id">Id of the document to open</param>
+        /// <returns>The document</returns>
+        public Document OpenDocument(int id)
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// Deletes a document from the database
+        /// </summary>
+        /// <param name="doc">The document to delete</param>
+        public void DeleteDocument(Document doc)
+        { 
+        }
     }
 }
