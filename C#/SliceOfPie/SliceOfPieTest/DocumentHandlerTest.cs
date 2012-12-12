8b12e080f6e5c88e35d805397646b2186b069879
Morten Holm Hvass <akkri777@gmail.com>
2012-12-12 13:10:10 +0100

    sharing of document work

--- C#/SliceOfPie/SliceOfPieTest/DocumentHandlerTest.cs
+++ C#/SliceOfPie/SliceOfPieTest/DocumentHandlerTest.cs
@@ -15,46 +15,50 @@ namespace SliceOfPieTest
         public void OfflineSynchronizationTest()
         {
             //creation of documents and user for testing
-            //User user1 = engine.userhandler.GetUser("mrT","1234");
-            //Document document1 = new Document(user1, 1, "");
-            //Document document2 = new Document(user1, 2, "");
-            //Document document3 = new Document(user1, 3, "");
+            User user1 = engine.userhandler.GetUser("mrT","1234");
+            Document document1 = new Document(user1, "tmp", "There", Permission.Permissions.Edit);
+            Document document2 = new Document(user1, "tmp", "There", Permission.Permissions.Edit);
+            Document document3 = new Document(user1, "tmp", "There", Permission.Permissions.Edit);
 
-            //Document document4 = new Document(user1, 2, "");
-            //Document document5 = new Document(user1, 3, "");
-            //Document document6 = new Document(user1, 4, "");
+            Document document4 = new Document(user1, "tmp", "There", Permission.Permissions.Edit);
+            Document document5 = new Document(user1, "tmp", "There", Permission.Permissions.Edit);
+            Document document6 = new Document(user1, "tmp", "There", Permission.Permissions.Edit);
 
-            //document2.lastChanged = new DateTime(2012, 12, 5);
-            //document3.lastChanged = new DateTime(2012, 12, 5);
-            //document4.lastChanged = new DateTime(2012, 12, 2);
-            //document5.lastChanged = new DateTime(2012, 12, 7);
+            document2.lastChanged = new DateTime(2012, 12, 5);
+            document3.lastChanged = new DateTime(2012, 12, 5);
+            document4.lastChanged = new DateTime(2012, 12, 2);
+            document5.lastChanged = new DateTime(2012, 12, 7);
 
-            //List<Document> userLocalDocuments = new List<Document>();
-            //List<Document> userServerDocuments = new List<Document>();
+            List<Document> userLocalDocuments = new List<Document>();
+            List<Document> userServerDocuments = new List<Document>();
 
-            //userLocalDocuments.Add(document1); //not on server
-            //userLocalDocuments.Add(document2); //most recent version
-            //userLocalDocuments.Add(document3); //oldest version
+            userLocalDocuments.Add(document1); //not on server
+            userLocalDocuments.Add(document2); //most recent version
+            userLocalDocuments.Add(document3); //oldest version
 
-            //userServerDocuments.Add(document4); //oldest version
-            //userServerDocuments.Add(document5); //most recent version
-            //userServerDocuments.Add(document6); //not local
+            userServerDocuments.Add(document4); //oldest version
+            userServerDocuments.Add(document5); //most recent version
+            userServerDocuments.Add(document6); //not local
 
-            //List<Document> testSet = engine.userhandler.docHandler.OfflineSynchronization(userLocalDocuments, userServerDocuments);
-            //Assert.AreEqual(4,testSet.Count);
-            //Assert.AreEqual(true, testSet.Contains(document1));
-            //Assert.AreEqual(true, testSet.Contains(document2));
-            //Assert.AreEqual(true, testSet.Contains(document5));
-            //Assert.AreEqual(true, testSet.Contains(document6));
+            List<Document> testSet = engine.userhandler.docHandler.OfflineSynchronization(userLocalDocuments, userServerDocuments);
+            Assert.AreEqual(4,testSet.Count);
+            Assert.AreEqual(true, testSet.Contains(document1));
+            Assert.AreEqual(true, testSet.Contains(document2));
+            Assert.AreEqual(true, testSet.Contains(document5));
+            Assert.AreEqual(true, testSet.Contains(document6));
         }
 
         [TestMethod]
         public void ShareDocumentTest()
         {
             User userOwner = engine.userhandler.GetUser("mrT", "1234");
-            User user1 = engine.userhandler.GetUser("chuckn", "1234");
+            User user1 = engine.userhandler.GetUser("bergar", "1234");
 
-            //Document testDoc = new Document();
+            Document testDoc = engine.userhandler.docHandler.OpenDocument(15, userOwner);
+
+            engine.userhandler.docHandler.ShareDocument(userOwner, testDoc, Permission.Permissions.Edit, user1);
+
+            Assert.AreEqual(true ,user1.documents.Contains(testDoc));
         }
     }
 }
